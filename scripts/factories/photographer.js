6a58b64f952f54bdc03294599487e4c4028fe39c
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2022-05-12 09:53:08 +0200

    recuperation des infos des photographes + afichage de ces infos

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,40 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait  } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const a = document.createElement('a');
+        a.setAttribute( "href" , "photographer.html?id=" + id );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute( "src" , picture);
+        img.setAttribute( "alt" , "photo de " + name);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        h2.setAttribute( "aria-label" , name);
+
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + ", " + country;
+
+        const h4 = document.createElement( 'h4' );
+        h4.textContent = tagline;
+
+        const p = document.createElement( 'p' );
+        p.textContent = price + '/jour'
+        
+        article.appendChild(a);
+
+        a.appendChild(img);
+        a.appendChild(h2);
+        a.appendChild(h3);
+        a.appendChild(h4);
+        a.appendChild(p);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, picture, getUserCardDOM }
 }
